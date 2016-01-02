1c26d8cc9a0576b9beffd06ac56fb376364cfd1b
Manuel Silva <msilva1@loopnet.com>
2016-01-02 10:23:48 -0800

    Initializing repository

---
+++ app/src/main/java/com/example/android/thinkr/ThinkrMainFragment.java
@@ -0,0 +1,22 @@
+package com.example.android.thinkr;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ThinkrMainFragment extends Fragment {
+
+    public ThinkrMainFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_thinkr_main, container, false);
+    }
+}
