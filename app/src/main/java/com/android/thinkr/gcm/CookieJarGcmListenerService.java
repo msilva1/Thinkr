1707fc8cfa3323cd93ef8f8b8f36271ebd11ea91
ryanclinton <Ryan.Clinton@antechimagingservices.com>
2016-01-02 16:22:55 -0800

    gym stuff

---
+++ app/src/main/java/com/android/thinkr/gcm/CookieJarGcmListenerService.java
@@ -0,0 +1,10 @@
+package com.android.thinkr.gcm;
+
+import com.google.android.gms.gcm.GcmListenerService;
+
+/**
+ * Created by ryanclinton on 1/2/16.
+ */
+public class CookieJarGcmListenerService extends GcmListenerService {
+
+}
