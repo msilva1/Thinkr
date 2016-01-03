e72ddeb85a2429ee189b7186166602bfa16d3f89
Manuel Silva <msilva1@loopnet.com>
2016-01-02 20:24:17 -0800

    Fixed sign-up and sign in is now required only once.

--- app/src/main/java/com/android/thinkr/ThinkrMain.java
+++ app/src/main/java/com/android/thinkr/ThinkrMain.java
@@ -1,22 +1,22 @@
 package com.android.thinkr;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class ThinkrMain extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         setContentView(R.layout.activity_thinkr_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -28,6 +28,11 @@ public class ThinkrMain extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        if (!Preferences.isSignedIn()) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
     }
 
     @Override
