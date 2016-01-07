4582d4adc4db6dbc16660e120aab092e11c4c8f1
Kent Lam <Kentative@live.com>
2016-01-07 08:04:40 -0800

    Added jdbc driver. Now moving to private branch.

--- server/src/com/bytes/hack/model/assignment/Question.java
+++ server/src/com/bytes/hack/model/assignment/Question.java
@@ -10,34 +10,81 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Question {
 
-	public enum Subject {
 
-		Undefined, 
-		Math, 
-		Science, 
-		History, 
-		Language, 
-		Other
-	}
-	
-	public enum AnswerType {
-		Text, 
-		Choice, 
-		Other
-	}
+    public enum Subject {
 
-	private String question;
-	private List<Answer> answers;	
-	private boolean isCompleted;
-	private int points;
-	private Answer answer;
-	
-	private Subject subject;
+        Undefined,
+        Math,
+        Science,
+        History,
+        Language,
+        Other
+    }
 
-	public Question() {
-		setSubject(Subject.Undefined);
-		setAnswer(new Answer());
-		setQuestion("This is a sample question?");
-	}
-	
+    public enum AnswerType {
+        Text,
+        Choice,
+        Other
+    }
+
+    private String question;
+    private List<Answer> answers;
+    private boolean isCompleted;
+    private Score score;
+    private Answer answer;
+    private Subject subject;
+
+    public Question() {
+        setSubject(Subject.Undefined);
+        setAnswer(new Answer());
+        setQuestion("This is a sample question?");
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public List<Answer> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(List<Answer> answers) {
+        this.answers = answers;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    public Score getScore() {
+        return score;
+    }
+
+    public void setScore(Score score) {
+        this.score = score;
+    }
+
+    public Answer getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(Answer answer) {
+        this.answer = answer;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
 }
---
+++ server/src/com/bytes/hack/model/assignment/Score.java
@@ -0,0 +1,7 @@
+package com.bytes.hack.model.assignment;
+
+/**
+ * Created by Kent on 1/6/2016.
+ */
+public class Score {
+}
